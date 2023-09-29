5d331e27ca5f0ba1cee633376374248009669073
EternityNightV <143103542+EternityNightV@users.noreply.github.com>
2023-09-29 22:43:29 +0300

    some modals + invite

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,8 +2,9 @@
 
 import { ServerWithMembersProfiles } from "@/type"
 import { MemberRole } from "@prisma/client"
-import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu"
-import { ChevronDown } from "lucide-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import { ChevronDown, LogOut, PlusCircle, Settings, TrashIcon, UserPlus, Users } from "lucide-react"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ServerHeaderProps {
     server : ServerWithMembersProfiles
@@ -14,9 +15,9 @@ export const ServerHeader =  ({
     server,
     role
 }:ServerHeaderProps) => {
-
+    const {onOpen} =  useModal();
     const isAdmin = role === MemberRole.ADMIN;
-    const idModerator = isAdmin || MemberRole.MODERATOR
+    const isModerator = isAdmin || MemberRole.MODERATOR
     return (
      <DropdownMenu>
         <DropdownMenuTrigger
@@ -32,8 +33,75 @@ export const ServerHeader =  ({
                 ml-auto
                 "/>
             </button>
-
         </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="
+                w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]
+                "
+                >
+                {isModerator && (
+                        <DropdownMenuItem
+                        onClick={()=> onOpen('invite',{server})}
+                        className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+                        >
+                            Invite People
+                            <UserPlus className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                        
+                    )}
+                {isAdmin && (
+                        <DropdownMenuItem
+                        onClick={()=>onOpen('editServer', {server})}
+                        className=" px-3 py-2 text-sm cursor-pointer"
+                        >
+                            Server Settings
+                            <Settings className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                        
+                    )}
+                {isAdmin && (
+                        <DropdownMenuItem
+                        onClick={()=>onOpen('members', {server})}
+                        className=" px-3 py-2 text-sm cursor-pointer"
+                        >
+                         Manage Members
+                         <Users className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                        
+                    )}
+                {isModerator && (
+                        <DropdownMenuItem
+                        className=" px-3 py-2 text-sm cursor-pointer"
+                        >
+                         Create Channel
+                         <PlusCircle className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                        
+                    )}    
+                {isModerator && (
+                    <DropdownMenuSeparator />
+                         
+                        
+                    )}  
+                {isAdmin && (
+                        <DropdownMenuItem
+                        className=" text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                        >
+                         Delete Server
+                         <TrashIcon className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                        
+                    )}  
+                {!isAdmin && (
+                        <DropdownMenuItem
+                        className=" text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                        >
+                         Leave Server
+                         <LogOut className="h-4 w-4 ml-auto"/>
+                        </DropdownMenuItem>
+                        
+                    )}           
+            </DropdownMenuContent>
      </DropdownMenu>  
     )
 }
